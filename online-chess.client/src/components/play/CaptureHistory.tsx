dd9a9ec3893a14f85d03ae165a4338fad475803e
Nash <naocenar@gmail.com>
2025-02-09 21:39:54 +0800

    capture pieces ui reflect

--- online-chess.client/src/components/play/CaptureHistory.tsx
+++ online-chess.client/src/components/play/CaptureHistory.tsx
@@ -1,38 +1,42 @@
-import { pieceImages, PieceNames } from '../../game/utilities/constants';
+import { useCallback } from 'react';
+import { PieceNames } from '../../game/utilities/constants';
 import useGameContext from '../../hooks/useGameContext';
+import useLocalStorage from '../../hooks/useLocalStorage';
 
 export default function CaptureHistory() {
     const { gameState } = useGameContext();
+    const { data: piece } = useLocalStorage("piece", "cburnett");
+    
+    const piecePath = `/src/assets/pieces/${piece}/`;
   
+    const imgFn = useCallback((piece: PieceNames) => {
+        let firstTwoChars = piece.slice(0, 2);
+
+        if (firstTwoChars === "wK"){
+            firstTwoChars = "wN";
+        } else if (firstTwoChars === "bK"){
+            firstTwoChars = "bN";
+        }
+
+        return `${piecePath}${firstTwoChars}.svg`;
+    }, []);
+
     return (
     <>
-        
         <div className='hstack'>
             {gameState.captureHistory.map((capture, idx) => {
                 const name = capture.uniqueName?.split("-")[0] as PieceNames
-                const svgUrl = `data:image/svg+xml;base64,${btoa(pieceImages[name])}`;
+                const imgSrc = imgFn(name);
+
                 return <div key={idx}>
                     <img 
-                        src={svgUrl} 
+                        src={imgSrc} 
                         alt={capture.uniqueName} 
                         style={{ width: 35, height: 35, }} 
                     />
                 </div>
             })}
         </div>
-        {/* <div className='hstack'>
-            {gameState.captureHistory.black.map((capture, idx) => {
-                const name = capture.pieceName.split("-")[0] as PieceNames
-                const svgUrl = `data:image/svg+xml;base64,${btoa(pieceImages[name])}`;
-                return <div key={idx}>
-                    <img 
-                        src={svgUrl} 
-                        alt={capture.pieceName} 
-                        style={{ width: 35, height: 35, }} 
-                    />
-                </div>
-            })}
-        </div> */}
     </>
   )
 }
